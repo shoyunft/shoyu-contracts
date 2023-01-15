f260fb7225962a59749a13bbf66b9ed96213ef88
0xMasayoshi <0xmasayoshi@protonmail.com>
2023-01-15 17:47:24 +0700

    feat: update to seaport 1.2

--- constants/addresses.ts
+++ constants/addresses.ts
@@ -6,8 +6,17 @@ export const CONDUIT_CONTROLLER_ADDRESS: { [chainId: number]: string } = {
   [ChainId.GÖRLI]: "0x00000000F9490004C11Cef243f5400493c00Ad63",
 };
 
-export const SEAPORT_ADDRESS: { [chainId: number]: string } = {
+export const SEAPORT_ADDRESS_1_1: { [chainId: number]: string } = {
   [ChainId.ETHEREUM]: "0x00000000006c3852cbef3e08e8df289169ede581",
   [ChainId.RINKEBY]: "0x00000000006c3852cbef3e08e8df289169ede581",
   [ChainId.GÖRLI]: "0x00000000006c3852cbEf3e08E8dF289169EdE581",
 };
+
+// TODO
+export const SEAPORT_ADDRESS_1_2: { [chainId: number]: string } = {
+  [ChainId.ETHEREUM]: "",
+  [ChainId.RINKEBY]: "",
+  [ChainId.GÖRLI]: "",
+};
+
+export const SEAPORT_ADDRESS = SEAPORT_ADDRESS_1_1;
